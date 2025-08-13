856fa14a58592373a324dfce40e8a23bf879377d
MohammedAnsal <ansalshaah786@gmail.com>
2025-08-13 16:04:02 +0530

    change coockie dev to production and also the vercel.json changed

--- backend/src/utils/cookie.ts
+++ backend/src/utils/cookie.ts
@@ -1,10 +1,19 @@
 import { Response } from "express";
 
+// export const setCookie = (res: Response, type: string, token: string) => {
+//   res.cookie(type, token, {
+//     httpOnly: true,
+//     secure: true,
+//     sameSite: "strict",
+//     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+//   });
+// };
+
 export const setCookie = (res: Response, type: string, token: string) => {
   res.cookie(type, token, {
     httpOnly: true,
     secure: true,
-    sameSite: "strict",
+    sameSite: "none",
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
   });
 };
